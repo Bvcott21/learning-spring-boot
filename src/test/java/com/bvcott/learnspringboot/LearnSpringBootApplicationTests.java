2532e84bb1a2ede79f138bd72cf3494f2b307822
edgar.afonso <edgar.afonso@fdmgroup.com>
2024-03-22 12:25:07 +0000

    Initialized project.

---
+++ src/test/java/com/bvcott/learnspringboot/LearnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bvcott.learnspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
